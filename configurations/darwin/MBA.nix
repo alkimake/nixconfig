9be35791b17a581fb8aa548d191e3573b54c118e
Alkim Ake Gozen <alkimake@pm.me>
2024-10-24 16:47:18 +0900

    machine: MBA: initialized

---
+++ configurations/darwin/MBA.nix
@@ -0,0 +1,29 @@
+# See /modules/darwin/* for actual settings
+# This file is just *top-level* configuration.
+{ flake, ... }:
+
+let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+in
+{
+  imports = [
+    self.darwinModules.default
+  ];
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  networking.hostName = "MBA";
+
+  # For home-manager to work.
+  # https://github.com/nix-community/home-manager/issues/4026#issuecomment-1565487545
+  users.users."ake".home = "/Users/ake";
+
+  # Enable home-manager for "ake" user
+  home-manager.users."ake" = {
+    imports = [ (self + /configurations/home/ake.nix) ];
+  };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
