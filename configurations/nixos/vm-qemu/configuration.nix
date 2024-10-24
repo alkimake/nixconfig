1c82407656522c57a066a827b0b72be32f2b89aa
Alkim Ake Gozen <alkimake@pm.me>
2024-10-24 16:46:27 +0900

    machine: vm-qemu: remove redundant settings

--- configurations/nixos/vm-qemu/configuration.nix
+++ configurations/nixos/vm-qemu/configuration.nix
@@ -45,21 +45,11 @@
     LC_TIME = "ko_KR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
@@ -89,9 +79,6 @@
     #  thunderbird
     ];
   };
-
-  # Install firefox.
-  programs.firefox.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
