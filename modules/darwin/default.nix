1f2cb2fedb64a59e9327e587066f353ed7ef0558
Alkim Ake Gozen <alkimake@pm.me>
2024-10-25 18:15:47 +0900

    darwin: dock: autohide

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -16,7 +16,7 @@ in {
   system = {
     defaults = {
       dock = {
-        # autohide = true;
+        autohide = true;
         # customize Hot Corners
         wvous-tl-corner = 2; # top-left - Mission Control
         # wvous-tr-corner = 13; # top-right - Lock Screen
