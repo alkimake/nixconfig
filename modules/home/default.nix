1e5afb05c14cc4d4b16c0959045ee2d7ca7e8f1e
Alkim Gozen <alkimake@pm.me>
2024-10-27 12:28:11 +0900

    home: use shared approach

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,18 +1,5 @@
-# A module that automatically imports everything else in the parent folder.
-# This module conditionally ignores certain files based on the operating system.
-
-{ pkgs, ... }:
-
 {
-  imports =
-    let
-      excludeDirs = if pkgs.stdenv.isDarwin then ["nixos-only"]
-                    else if pkgs.stdenv.isLinux then ["darwin-only"]
-                    else [];
-      files = builtins.attrNames (builtins.readDir ./.);
-      filteredFiles = builtins.filter
-        (fn: fn != "default.nix" && !(builtins.elem fn excludeDirs))
-        files;
-    in
-    map (fn: ./${fn}) filteredFiles;
+  imports = [
+    ./shared
+  ]
 }
