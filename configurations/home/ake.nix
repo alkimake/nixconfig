e7a5e8107aaaa08eaab2838a909337883d23ebe0
Alkim Ake Gozen <alkimake@pm.me>
2024-10-27 13:10:21 +0900

    broken: try to load homeModules system spesific

--- configurations/home/ake.nix
+++ configurations/home/ake.nix
@@ -1,21 +1,34 @@
-{ flake, pkgs, lib, ... }:
-let
+{
+  flake,
+  pkgs,
+  lib,
+  ...
+}: let
   inherit (flake) inputs;
   inherit (inputs) self;
+  inherit (pkgs.stdenv) isDarwin;
   nixvim' = inputs.nixvim.packages.${pkgs.system}.default;
   nvim = nixvim'.extend {
     # config.theme = lib.mkForce "jellybeans";
   };
-
-in
-{
+  systemBaseHomeModules = (
+    if isDarwin
+    then self.homeModules.darwin
+    else self.homeModules.nixos
+  );
+in {
   home.packages = [
     nvim
   ];
   imports = [
     self.homeModules.default
+    systemBaseHomeModules
   ];
   home.username = "ake";
-  home.homeDirectory = lib.mkDefault "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/ake";
+  home.homeDirectory = lib.mkDefault "/${
+    if isDarwin
+    then "Users"
+    else "home"
+  }/ake";
   home.stateVersion = "22.11";
 }
