1e6e918f569b5ae45136b7209849bcd99687603b
Alkim Ake Gozen <alkimake@pm.me>
2024-10-28 17:49:13 +0900

    darwin: brews: mas is included
    But Proton pass for Safari could not be installed

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -4,7 +4,14 @@ in {
     enable = true;
     casks = [
       "spotify"
+      "proton-pass"
     ];
+    brews = [
+      "mas"
+    ];
+    masApps = {
+      # "Proton Pass for Safari" = 2087098929;
+    };
     onActivation.cleanup = "zap";
   };
 }
