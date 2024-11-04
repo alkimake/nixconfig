e73877bcdeb03228ef8c77d00917268a73f96c4b
Alkim Ake Gozen <alkimake@pm.me>
2024-11-04 11:09:38 +0900

    home: theme: more catppuccin supports

--- modules/home/shared/theme.nix
+++ modules/home/shared/theme.nix
@@ -1,7 +1,18 @@
-{flake, ...}: {
+{flake, ...}: let
+  flavor = "mocha";
+in {
   imports = [
     flake.inputs.nix-colors.homeManagerModules.default
     flake.inputs.catppuccin.homeManagerModules.catppuccin
   ];
   colorScheme = flake.inputs.nix-colors.colorSchemes.catppuccin-mocha;
+  catppuccin.flavor = flavor;
+  programs = {
+    lazygit.catppuccin.enable = true;
+    git.delta.catppuccin.enable = true;
+    btop.catppuccin.enable = true;
+    fzf.catppuccin.enable = true;
+    starship.catppuccin.enable = true;
+    zsh.syntaxHighlighting.catppuccin.enable = true;
+  };
 }
