497ad47cf328b72e60bf388f5fb84c5bd355623f
Alkim Ake Gozen <alkimake@pm.me>
2024-11-16 12:12:38 +0900

    packages: golang

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -1,10 +1,13 @@
 # Common to Linux & darwin
-{
+{pkgs, ...}: {
   imports = [
     ./shared/fonts.nix
     ./shared/nix.nix
     ./shared/cachix.nix
     ./shared/tailscale.nix
     ./shared/zsh.nix
- ];
+  ];
+  environment.systemPackages = with pkgs; [
+    go
+  ];
 }
