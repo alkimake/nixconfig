4486bde9e73725e2550697a70b040fe126c3d5a1
Alkim Ake Gozen <alkimake@pm.me>
2024-11-23 14:23:19 +0900

    home: zsh: no need limactl check on zsh inital

--- modules/home/darwin/zsh.nix
+++ modules/home/darwin/zsh.nix
@@ -7,17 +7,5 @@ _: {
     export PATH="/opt/homebrew/bin:$PATH"
 
 
-    # Lima Docker context setup
-    if command -v limactl >/dev/null 2>&1; then
-      # Start Lima if it's not running
-      if ! limactl list | grep -q "docker.*Running"; then
-        limactl start template://docker
-      fi
-
-      # Set up Docker context for Lima
-      if ! docker context ls | grep -q "lima-docker"; then
-        docker context create lima-docker --docker "host=unix://$${HOME}/.lima/docker/sock/docker.sock"
-      fi
-    fi
   '';
 }
