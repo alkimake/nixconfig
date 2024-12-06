b83859ece82822929e4793ae274d41b190aa0b05
Alkim Ake Gozen <alkimake@pm.me>
2024-12-06 14:31:17 +0900

    darwin: sketchybar japanese keyboard support

--- modules/home/darwin/sketchybar/config/plugins/get_input_source.sh
+++ modules/home/darwin/sketchybar/config/plugins/get_input_source.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Read the plist data
 plist_data=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources)
-current_input_source=$(echo "$plist_data" | plutil -convert xml1 -o - - | grep -A1 'KeyboardLayout Name' | tail -n1 | cut -d '>' -f2 | cut -d '<' -f1)
+input_info=$(echo "$plist_data" | plutil -convert xml1 -o - -)
+current_input_source=$(echo "$input_info" | grep -A1 'KeyboardLayout Name' | tail -n1 | cut -d '>' -f2 | cut -d '<' -f1)
 
 if [ "$current_input_source" = "ABC" ]; then
     sketchybar --set input_source icon="󱌯"
-else
+elif echo "$input_info" | grep -q "com.apple.inputmethod.Korean"; then
     sketchybar --set input_source icon="🇰🇷"
+elif echo "$input_info" | grep -q "com.apple.inputmethod.Japanese"; then
+    sketchybar --set input_source icon="🇯🇵"
 fi
