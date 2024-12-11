594771e5473090949280f7ff49e63fef703c0d62
Alkim Ake Gozen <alkim@lviscorp.com>
2024-12-11 14:40:28 +0900

    home: tmux: resize mode is added
    it does not work constant, but only first key stroke

--- modules/home/shared/tmux.nix
+++ modules/home/shared/tmux.nix
@@ -39,7 +39,8 @@ in {
     extraConfig = ''
       set -ag terminal-overrides ",xterm-256color:RGB"
       set-option -g status-position top
-      set-option -sg escape-time 10
+      # set-option -sg escape-time 10
+      set-option -g repeat-time 1000
 
       # Start windows and panes at 1, not 0
       set -g base-index 1
@@ -52,7 +53,7 @@ in {
       bind -n S-right next-window
 
       # Easier reload of config
-      bind r source-file ~/.config/tmux/tmux.conf
+      bind R source-file ~/.config/tmux/tmux.conf
 
       # Change splits to match nvim and easier to remember
       # Open new split at cwd of current split
@@ -71,6 +72,41 @@ in {
       unbind p
       bind p paste-buffer
 
+      # Enable resize mode with prefix + r
+      bind r switch-client -T resize
+
+      # Resize mode bindings
+      # Use h,j,k,l for fine adjustments (1 cell)
+      bind -T resize h resize-pane -L
+      bind -T resize j resize-pane -D
+      bind -T resize k resize-pane -U
+      bind -T resize l resize-pane -R
+
+      # Use H,J,K,L for larger adjustments (5 cells)
+      bind -T resize H resize-pane -L 5
+      bind -T resize J resize-pane -D 5
+      bind -T resize K resize-pane -U 5
+      bind -T resize L resize-pane -R 5
+
+      # Use arrow keys for fine adjustments
+      bind -T resize Left resize-pane -L
+      bind -T resize Down resize-pane -D
+      bind -T resize Up resize-pane -U
+      bind -T resize Right resize-pane -R
+
+      # Use Ctrl + arrow keys for larger adjustments
+      bind -T resize C-Left resize-pane -L 5
+      bind -T resize C-Down resize-pane -D 5
+      bind -T resize C-Up resize-pane -U 5
+      bind -T resize C-Right resize-pane -R 5
+
+      # Press escape or enter to exit resize mode
+      bind -T resize Escape switch-client -T root
+      bind -T resize Enter switch-client -T root
+
+      # Show visual indicator when in resize mode
+      set-hook -g after-bind "if -F '#{==:#{client-key-table},resize}' 'set -g status-style bg=red' 'set -g status-style bg=default'"
+      set-hook -g pane-mode-changed "if -F '#{==:#{client-key-table},resize}' 'set -g status-style bg=red' 'set -g status-style bg=default'"
 
     '';
   };
