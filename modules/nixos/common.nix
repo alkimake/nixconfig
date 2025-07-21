fffc737b46728c1bbd1efe1b2e9b22042c64e52c
Alkim Ake Gozen <alkim@lviscorp.com>
2025-07-21 15:26:58 +0900

    nixos: update common packages and disable posting due to build issues

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -7,7 +7,13 @@
   environment.systemPackages = with pkgs; [
     go
     awscli2
-    posting
+    # posting  # Disabled due to textual build issues with tree_sitter.Query compatibility
+    # TODO: Re-enable when textual package is fixed or use alternative Mastodon client
     devenv
+
+    nodejs
+    yarn
+    node2nix
+    bun
   ];
 }
